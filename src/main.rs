cbf7323231653e2c782c71d704017b1a96e8201b
franek <franeklubi@gmail.com>
2020-12-25 22:08:58 +0100

    Add the initial pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,62 @@
+use std::io::{self, Read, Write};
 use std::env;
 use std::process;
+use std::fs::File;
+use exitcode;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	println!("{:?}", args);
+
 	let args_len = args.len();
+
 	if args_len > 2 {
-		println!("Usage: jlox [script]");
-		process::exit(64);
+		println!("Usage: jlox [script] <-- notice, only one script dummy");
+		process::exit(exitcode::USAGE);
 	} else if args_len == 2 {
-		run_file(&args[1]);
+		match run_file(&args[1]) {
+			Err(err) => {
+				println!("{}", err);
+				process::exit(exitcode::IOERR);
+			},
+			_ => (),
+		};
 	} else {
-		run_prompt();
+		match run_prompt() {
+			Err(err) => {
+				println!("{}", err);
+				process::exit(exitcode::OSERR);
+			},
+			_ => (),
+		};
 	}
 }
 
-fn run_file(filename: &String) {
-	println!("running file: {}", filename);
+fn run_file(path: &String) -> Result<(), io::Error> {
+	let mut f = File::open(path)?;
+
+	let mut buffer = String::new();
+
+	f.read_to_string(&mut buffer)?;
+
+	run(&buffer);
+
+	Ok(())
 }
 
-fn run_prompt() {
-	println!("heja prompt");
+fn run_prompt() -> Result<(), io::Error> {
+	println!("========\nPROMPT:");
+
+	loop {
+		print!(">>> ");
+		io::stdout().flush()?;
+
+		let mut buffer = String::new();
+		io::stdin().read_line(&mut buffer)?;
+
+		run(&buffer);
+	}
+}
+
+fn run(source: &String) {
+	print!("{}", source);
 }
