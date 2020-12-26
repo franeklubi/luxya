f47519fa21a8f7a4a736c274f635752edd82e596
franek <franeklubi@gmail.com>
2020-12-26 19:31:11 +0100

    Get rid of rightward drift

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,21 @@ fn main() {
 	let args_len = args.len();
 
 	if args_len > 2 {
+		// TODO: exec all scripts in the order they're passed maybe???
 		println!("Usage: jlox [script] <-- notice, only one script dummy");
 		process::exit(exitcode::USAGE);
 	} else if args_len == 2 {
-		match jlox::run_file(&args[1]) {
-			Err(err) => {
-				match err {
-					jlox::RunError::IO(err) => {
-						println!("{}", err);
-						process::exit(exitcode::IOERR);
-					},
-					jlox::RunError::EXEC => {
-						println!("Errors while executing the file");
-						process::exit(exitcode::DATAERR);
-					}
+		if let Err(err) = jlox::run_file(&args[1]) {
+			match err {
+				jlox::RunError::IO(err) => {
+					println!("{}", err);
+					process::exit(exitcode::IOERR);
+				},
+				jlox::RunError::EXEC => {
+					println!("Errors while executing the file");
+					process::exit(exitcode::DATAERR);
 				}
-			},
-			_ => (),
+			}
 		};
 	} else {
 		match jlox::run_prompt() {
