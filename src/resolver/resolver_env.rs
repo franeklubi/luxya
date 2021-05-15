38b05f45a0c28618eecebbe11143b28a4c1f3bd3
franek <franeklubi@gmail.com>
2021-05-15 20:43:06 +0200

    Remove comments

--- src/resolver/resolver_env.rs
+++ src/resolver/resolver_env.rs
@@ -112,14 +112,6 @@ impl ResolverEnvironment {
 			|| unwrap_enclosing!(self)
 				.as_ref()
 				.map_or(false, |enclosing| enclosing.exists(name))
-
-		// if unwrap_scope!(self).get(name).is_some() {
-		// 	true
-		// } else if let Some(enclosing) = unwrap_enclosing!(self) {
-		// 	enclosing.exists(identifier)
-		// } else {
-		// 	false
-		// }
 	}
 
 	pub fn resolve_nest_level(
