42b7fa823e23256a76ce5825de13a941dea82897
franek <franeklubi@gmail.com>
2021-05-16 12:37:34 +0200

    Change package name

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,19 @@ fn main() {
 
 	if args.len() > 1 {
 		for arg in args.iter().skip(1) {
-			match jlox::run_file(&arg) {
-				Err(jlox::RunError::Io(err)) => {
+			match luxya::run_file(&arg) {
+				Err(luxya::RunError::Io(err)) => {
 					println!("{}", err);
 					process::exit(exitcode::IOERR);
 				}
-				Err(jlox::RunError::Exec) => {
+				Err(luxya::RunError::Exec) => {
 					println!("Errors while executing {}", arg);
 					process::exit(exitcode::DATAERR);
 				}
 				_ => (),
 			}
 		}
-	} else if let Err(err) = jlox::run_prompt() {
+	} else if let Err(err) = luxya::run_prompt() {
 		println!("{}", err);
 		process::exit(exitcode::OSERR);
 	}
